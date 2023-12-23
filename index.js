a82938a7787ada57faa2d876741b36cea8051385
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-12-23 18:54:38 +0000

    Creates classes

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// TEST //
+const emp1 = new Manager("Nick Harvard", "nharv@grmail.com", 1);
+const emp2 = new Engineer("Dim Gian", "dg92@gmail.com", "jimmygian");
+const emp3 = new Engineer("Nick MIT", "nharv@grmail.com", "nickmit22");
+const emp4 = new Intern("Jack Stanford", "nharv@grmail.com", "Harvard");
+
+console.log(emp1, emp1.getRole())
+console.log(emp2, emp2.getRole())
+console.log(emp3, emp3.getRole())
+console.log(emp4, emp4.getRole())
+// --- //
